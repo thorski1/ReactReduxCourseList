4ffb56c7a53ce4576b05ba7767adc1e17a2c40d2
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-11-25 15:09:31 -0500

    initial commit

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageNotFound = () => <h1>OOPS! PAGE NOT FOUND...</h1>;
+
+export default PageNotFound;
---
+++ src/components/common/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+	const activeStyle = { color: "#F15B2A" };
+
+	return (
+		<nav>
+			<NavLink to="/" activeStyle={activeStyle} exact>
+				Home
+			</NavLink>
+			{" | "}
+			<NavLink to="/about" activeStyle={activeStyle} exact>
+				About
+			</NavLink>
+			{" | "}
+			<NavLink
+				to="/courses"
+				activeStyle={activeStyle}
+				exact
+			>
+				Courses
+			</NavLink>
+		</nav>
+	);
+};
+
+export default Header;
